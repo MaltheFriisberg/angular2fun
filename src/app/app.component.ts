9ea62ec09017977d3be2433f98aa78c939e6cf43
Malthe <malthef@gmail.com>
2017-08-14 21:24:13 +0200

    made it to step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+/* how to import ts class */
+import { Recipe } from '../app/recipe'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: `<h1>{{title}}</h1><h2>{{recipe}} details!</h2>`,
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{recipe.name}} details!</h2>
+  <div><label>id: </label>{{recipe.id}}</div>
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]="recipe.name" placeholder="name">
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'International Recipe Database';
-  recipe = "Chili sin carne";
+  recipe : Recipe = { id : 1, name : "Chili sin carne"}; 
 }
