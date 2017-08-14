ac22ccfa3cb2814f12f268c889fd4aeb48061ada
Malthe <malthef@gmail.com>
2017-08-14 09:48:57 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{recipe}} details!</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'International Recipe Database';
+  recipe = "Chili sin carne";
 }
