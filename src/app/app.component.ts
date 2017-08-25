9ece0ae5f66a25f50d99b658576ac90d2c81822f
Malthe <malthef@gmail.com>
2017-08-25 09:46:19 +0200

    The app no longer fails and the list of names displays again in the browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,23 +20,78 @@ const RECIPES: Recipe[] = [
   templateUrl: './app.component.html',
   template: `
   <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-  <li *ngFor="let recipe of recipes">
+  <h2>My Recipes</h2>
+  <ul class="recipes">
+  <li *ngFor="let recipe of recipes" (click)="onSelect(recipe)">
     <span class="badge">{{recipe.id}}</span> {{recipe.name}}
   </li>
   </ul>
-  <h2>{{recipe.name}} details!</h2>
-  <div><label>id: </label>{{recipe.id}}</div>
+  <div *ngIf="selectedRecipe">
+  <h2>{{selectedRecipe.name}} details!</h2>
+  <div><label>id: </label>{{selectedRecipe.id}}</div>
   <div>
   <label>name: </label>
-  <input [(ngModel)]="recipe.name" placeholder="name">
+  <input [(ngModel)]="selectedRecipe.name" placeholder="name">
+  </div>
   </div>
   `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .recipes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .recipes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .recipes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .recipes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .recipes .text {
+    position: relative;
+    top: -3px;
+  }
+  .recipes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'International Recipe Database';
-  recipe : Recipe = { id : 1, name : "Chili sin carne"}; 
+  selectedRecipe : Recipe; 
   recipes = RECIPES;
+
+  onSelect(recipe : Recipe): void {
+    this.selectedRecipe = recipe;
+  } 
 }
