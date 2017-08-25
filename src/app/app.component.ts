5bb3efc514e8b5c6b591038bf06c25bf8cecd695
Malthe <malthef@gmail.com>
2017-08-25 08:52:25 +0200

    added hardcoded RECIPES array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 /* how to import ts class */
 import { Recipe } from '../app/recipe'; 
 
+const RECIPES: Recipe[] = [
+  { id: 11, name: 'Lasagna' },
+  { id: 12, name: 'Sushi' },
+  { id: 13, name: 'Cabonarra' },
+  { id: 14, name: 'Pizza' },
+  { id: 15, name: 'Vegan Salad' },
+  { id: 16, name: 'Tomato Salad' },
+  { id: 17, name: 'The Last Supper' },
+  { id: 18, name: 'Grilled Salmon' },
+  { id: 19, name: 'Indian Curry' },
+  { id: 20, name: 'Fish Soup' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,4 +32,5 @@ import { Recipe } from '../app/recipe';
 export class AppComponent {
   title = 'International Recipe Database';
   recipe : Recipe = { id : 1, name : "Chili sin carne"}; 
+  recipes = RECIPES;
 }
