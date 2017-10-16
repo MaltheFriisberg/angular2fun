cdb05b23e48c5d86318bf5a1d53baa001abd9dc8
Malthe <malthef@gmail.com>
2017-10-16 22:34:15 +0200

    completed sharing of the recipesService, implemented dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,94 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-/* how to import ts class */
-import { Recipe } from '../app/recipe'; 
-import {RecipeService} from './recipe.service';
-
+import { Component } from "@angular/core";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>My Recipes</h2>
-  <ul class="recipes">
-  <li *ngFor="let recipe of recipes"
-    [class.selected]="recipe === selectedRecipe" 
-    (click)="onSelect(recipe)  ">
-    <span class="badge">{{recipe.id}}</span> {{recipe.name}}
-  </li>
-  </ul>
-  <recipe-detail [recipe]="selectedRecipe"></recipe-detail>
-  `,
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .recipes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .recipes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .recipes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .recipes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .recipes .text {
-    position: relative;
-    top: -3px;
-  }
-  .recipes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-providers: [RecipeService]
+selector:'app-root',
+template: `
+<h1>{{title}}</h1>
+<nav>
+<a routerLink="/dashboard">Dashboard</a>
+<a routerLink="/recipes">Recipes</a>
+</nav>
+<router-outlet></router-outlet>
+ 
+`,
+
 })
-export class AppComponent implements OnInit{
-  
 
-  /*Dependency injecting the recipeService */
-  constructor(private recipeService : RecipeService) {}
-  
-  title = 'International Recipe Database';
-  selectedRecipe : Recipe; 
-  recipes : Recipe[];
+export class AppComponent {
 
-  onSelect(recipe : Recipe): void {
-    this.selectedRecipe = recipe;
-  } 
-  getRecipes(): void {
-    /*Handling the async callback with .then */
-    this.recipeService.getRecipes().then(recipes => this.recipes = recipes);
-  }
-  ngOnInit(): void {
-    this.getRecipes();
-  }
+    title = 'International Recipe Database';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,42 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { RecipeDetailComponent } from "./recipe-detail.component";
 import { AppComponent } from './app.component';
+import {RecipesComponent} from './recipes.component';
+import { RecipeService } from './recipe.service';
+import {RouterModule} from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path : 'recipes',
+        component: RecipesComponent
+      },
+      {
+        path: 'dashboard',
+        component : DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    
+    ]),
 
     FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
